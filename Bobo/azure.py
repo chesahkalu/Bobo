bef16bef83607c3bbec308ff0a01c6a3cbe5f77d
Chesachi Victor Kalu <chesahkalu@gmail.com>
2024-03-21 21:00:10 +0100

    blod storage mounted to app service and normal media url used on production file

--- Bobo/azure.py
+++ Bobo/azure.py
@@ -60,19 +60,8 @@ CACHES = {
 }
 
 
-# Configure Azure Blob Storage for media files
-conn_str2 = os.environ.get('AZURE_STORAGE_CONNECTIONSTRING')
-conn_str2_params = {pair.split('=')[0]: '='.join(pair.split('=')[1:]) for pair in conn_str2.split(';')}
-
-
-
-AZURE_ACCOUNT_NAME = conn_str2_params.get['AccountName']
-AZURE_ACCOUNT_KEY = conn_str2_params.get['AccountKey']
-
-AZURE_CONTAINER = "media"
-DEFAULT_FILE_STORAGE = 'storages.backends.azure_storage.AzureStorage'
-AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
-MEDIA_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{AZURE_CONTAINER}/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
