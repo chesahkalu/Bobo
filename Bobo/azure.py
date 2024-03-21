3b593714af0c7e8714ef92737a1593a1f2f0f5c0
Chesachi Victor Kalu <chesahkalu@gmail.com>
2024-03-21 12:33:51 +0100

    azure production file updates with storage credentials for storing baby media files

--- Bobo/azure.py
+++ Bobo/azure.py
@@ -59,19 +59,19 @@ CACHES = {
     }
 }
 
-"""
+
 # Configure Azure Blob Storage for media files
 conn_str2 = os.environ['AZURE_STORAGE_CONNECTIONSTRING']
 conn_str2_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 AZURE_CONTAINER = "media"
 
-AZURE_ACCOUNT_NAME = conn_str2_params('AccountName')
-AZURE_ACCOUNT_KEY = conn_str2_params('AccountKey')
+AZURE_ACCOUNT_NAME = conn_str2_params['AccountName']
+AZURE_ACCOUNT_KEY = conn_str2_params['AccountKey']
 
 DEFAULT_FILE_STORAGE = 'storages.backends.azure_storage.AzureStorage'
 AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
 MEDIA_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{AZURE_CONTAINER}/'
-"""
+
 
 
 """
