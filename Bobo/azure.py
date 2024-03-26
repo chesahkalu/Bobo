1b8ae982ae2b52c14dea04332bed715b5e096483
Chesachi Victor Kalu <chesahkalu@gmail.com>
2024-03-26 17:31:46 +0100

    storage settings for production updated

--- Bobo/azure.py
+++ Bobo/azure.py
@@ -59,7 +59,16 @@ CACHES = {
     }
 }
 
+conn_str2 = os.environ['AZURE_BLOB_CONNECTIONSTRING']
+conn_str2_params = {pair.split('=')[0]: '='.join(pair.split('=')[1:]) for pair in conn_str2.split(';')}
 
+AZURE_ACCOUNT_NAME = conn_str2_params['AccountName']
+AZURE_ACCOUNT_KEY = conn_str2_params['AccountKey']
+
+AZURE_CONTAINER = "pictures"
+DEFAULT_FILE_STORAGE = 'storages.backends.azure_storage.AzureStorage'
+AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
+MEDIA_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{AZURE_CONTAINER}/'
 
 """
 # STATIC AND MEDIA FILES CONFIGURATION
